c00b51207fdf160d631f50ae743e14cf347bdf82
JonathanKimchi <59551654+JonathanKimchi@users.noreply.github.com>
2021-06-14 19:38:31 -0400

    test

--- public/index2.js
+++ public/index2.js
@@ -67,10 +67,7 @@ function loginUI()
     signInFlow: 'popup',
     signInOptions: [
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     // Other config options...
   });
