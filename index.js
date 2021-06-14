390101c34d3ad31ae13f522f4a128797ec25c97d
JonathanKimchi <59551654+JonathanKimchi@users.noreply.github.com>
2021-06-14 11:24:26 -0400

    test

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var app = express();
-var firebase = require('firebase/app');
-require("firebase/auth");
 app.set("view engine","ejs");
 app.use(express.static(__dirname+'/public'));
-var firebaseConfig = {
-    apiKey: "AIzaSyBhT_UgYabv0UZ0KoHw082biu2S4SEyNbw",
-    authDomain: "onetimepassword-f8dc6.firebaseapp.com",
-    projectId: "onetimepassword-f8dc6",
-    storageBucket: "onetimepassword-f8dc6.appspot.com",
-    messagingSenderId: "344320674572",
-    appId: "1:344320674572:web:b9cf6aeede0c7289aad729",
-    measurementId: "G-WNQW4QSNM1"
-};
+
 app.get('/', (req,res)=> {
     res.render("index");
 });
@@ -20,7 +10,7 @@ app.get('/', (req,res)=> {
 app.get('/dashboard', (req,res)=> 
 {
         // Initialize Firebase
-        res.render("home");
+        res.render("dashboard");
 });
 
 app.listen(3000,()=>
